fd3520def152e4642a0cfc91a27d2af8a929d6e6
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-11-19 19:45:00 +0100

    feat(netlify): tryout netlify SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import tailwindcss from 'tailwindcss';
 import postcssImport from 'postcss-import';
 import postcssNesting from 'tailwindcss/nesting';
 import autoprefixer from 'autoprefixer';
-// import netlify from '@astrojs/netlify/functions';
+// import netlify from '@astrojs/netlify/static'
 import alpine from '@astrojs/alpinejs';
 import solid from '@astrojs/solid-js';
 import astroI18next from 'astro-i18next';
 import sitemap from '@astrojs/sitemap';
-import vercelStatic from '@astrojs/vercel/static';
+// import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercelStatic(),
+  // adapter: vercelStatic(),
   integrations: [
     sitemap({
       i18n: {
