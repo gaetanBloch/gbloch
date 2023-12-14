ff4118a1d648776887e053aaa85b8d569cf66e6e
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-12-14 08:21:50 +0100

    chore(vercel): push to build

--- .vercel/output/static/_astro/hoisted.7353c3b0.js
+++
@@ -1,9 +0,0 @@
-import { m as o } from './module.esm.0b549d28.js';
-window.Alpine = o;
-o.start();
-function d(n) {
-  const e = n ? 'dark' : 'light',
-    t = window.document.documentElement;
-  t.classList.remove(e === 'dark' ? 'light' : 'dark'), t.classList.add(e);
-}
-window.changeTheme = d;
--- .vercel/output/static/_astro/page.4f13a1c6.js
+++
@@ -1,3 +0,0 @@
-import { m } from './module.esm.0b549d28.js';
-window.Alpine = m;
-m.start();
